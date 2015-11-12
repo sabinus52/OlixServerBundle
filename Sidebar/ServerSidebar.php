825a14e630783b6386ceccabbd3a35c158679327
Olivier <sabinus52@gmail.com>
2015-11-12 23:17:40 +0100

    Ajout du menu complémentaire pour la sidebar

---
+++ Sidebar/ServerSidebar.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Sidebar pour la gestion des services du serveur
+ *
+ * @author Olivier <sabinus52@gmail.com>
+ *
+ * @package Olix
+ * @subpackage ServerBundle
+ */
+
+namespace Olix\ServerBundle\Sidebar;
+
+use Olix\AdminBundle\Factory\SidebarItem;
+use Symfony\Component\Security\Core\SecurityContext;
+
+
+class ServerSidebar
+{
+
+    /**
+     * @var \Symfony\Component\Security\Core\SecurityContext
+     */
+    protected $context;
+
+
+    /**
+     * Constructeur
+     * 
+     * @param SecurityContext $context
+     */
+    public function __construct(SecurityContext $context)
+    {
+        $this->context = $context;
+    }
+
+
+    /**
+     * Construit le menu supplémentaire de la gestion des services du serveur
+     * 
+     * @param SidebarItem $sidebar Sidebar d'origine à completer
+     */
+    public function build(SidebarItem $sidebar)
+    {
+        if (!$this->context->isGranted('ROLE_SUPER_ADMIN')) return;
+        
+        $server = $sidebar->addChild('olix_server', array(
+            'label' => 'Gestion du serveur',
+            'icon'  => 'fa fa-server fa-fw',
+        ));
+    }
+
+}
